82277da47e39ccf3002eb36187c49758146aa7fa
alireza787b <p30planets@gmail.com>
2023-07-13 07:54:10 +0000

    enhancing coordinator for swarm

--- coordinator.py
+++ coordinator.py
@@ -47,6 +47,7 @@ import logging
 import csv
 import glob
 import requests
+from geographiclib.geodesic import Geodesic
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -111,6 +112,10 @@ class DroneConfig:
         self.velocity = {'vel_n': 0, 'vel_e': 0, 'vel_d': 0}
         self.battery = 0
         self.last_update_timestamp = 0
+        self.position_setpoint_LLA = {'lat': 0, 'long': 0, 'alt': 0}
+        self.position_setpoint_NED = {'north': 0, 'east': 0, 'down': 0}
+        self.velocity_setpoint_NED = {'north': 0, 'east': 0, 'down': 0}
+        self.target_drone = None
 
     def get_hw_id(self):
         hw_id_files = glob.glob("*.hwID")
@@ -186,6 +191,83 @@ class DroneConfig:
         
         print("Swarm configuration not found.")
         return None
+    def calculate_setpoints(self):
+        self.find_target_drone()
+
+        if self.target_drone:
+            self.calculate_position_setpoint_LLA()
+            self.calculate_position_setpoint_NED()
+            self.calculate_velocity_setpoint_NED()
+        elif self.swarm.get('follow') == 0:
+            print(f"Drone {self.hw_id} is a master drone and not following anyone.")
+        else:
+            print(f"No drone to follow for drone with hw_id: {self.hw_id}")
+
+    def find_target_drone(self):
+        # find which drone it should follow
+        follow_hw_id = self.swarm.get('follow', None)
+        if follow_hw_id == 0:
+            print(f"Drone {self.hw_id} is a master drone and not following anyone.")
+        elif follow_hw_id == self.hw_id:
+            print(f"Drone {self.hw_id} is set to follow itself. This is not allowed.")
+        else:
+            self.target_drone = drones.get(follow_hw_id, None)
+            if self.target_drone:
+                print(f"Drone {self.hw_id} is following drone {self.target_drone.hw_id}")
+            else:
+                print(f"No target drone found for drone with hw_id: {self.hw_id}")
+
+    def calculate_position_setpoint_LLA(self):
+        # find its setpoints
+        offset_n = self.swarm.get('offset_n', 0)
+        offset_e = self.swarm.get('offset_e', 0)
+        offset_alt = self.swarm.get('offset_alt', 0)
+
+        # find its target drone position
+        if self.target_drone:
+            # Calculate new LLA with offset
+            geod = Geodesic.WGS84  # define the WGS84 ellipsoid
+            g = geod.Direct(self.target_drone.position['lat'], self.target_drone.position['long'], 90, offset_e)
+            g = geod.Direct(g['lat2'], g['lon2'], 0, offset_n)
+
+            self.position_setpoint_LLA = {
+                'lat': g['lat2'],
+                'long': g['lon2'],
+                'alt': self.target_drone.position['alt'] - offset_alt,  # considering negative for NED
+            }
+
+            # The above method calculates a new LLA coordinate by moving a certain distance 
+            # in the north (latitude) and east (longitude) direction. This is an approximation, 
+            # and it assumes that a degree of latitude and longitude represents the same distance 
+            # everywhere on the globe. For small distances, this should be a reasonable approximation, 
+            # but for larger distances, this approximation may not hold true. If more accuracy is 
+            # required, one should use a more advanced method or library that can account for the 
+            # curvature of the earth.
+
+            print(f"Position setpoint for drone {self.hw_id}: {self.position_setpoint_LLA}")
+        else:
+            print(f"No target drone found for drone with hw_id: {self.hw_id}")
+
+    def calculate_position_setpoint_NED(self):
+        # Conversion to local NED coordinates would go here
+        # For simplicity, let's assume we already have a function convert_LLA_to_NED() to perform this conversion
+        if self.target_drone:
+            self.position_setpoint_NED = self.convert_LLA_to_NED(self.position_setpoint_LLA)
+            print(f"NED Position setpoint for drone {self.hw_id}: {self.position_setpoint_NED}")
+        else:
+            print(f"No target drone found for drone with hw_id: {self.hw_id}")
+
+    def calculate_velocity_setpoint_NED(self):
+        # velocity setpoints is exactly the same as the target drone velocity
+        if self.target_drone:
+            self.velocity_setpoint_NED = self.target_drone.velocity
+            print(f"NED Velocity setpoint for drone {self.hw_id}: {self.velocity_setpoint_NED}")
+        else:
+            print(f"No target drone found for drone with hw_id: {self.hw_id}")
+
+    def convert_LLA_to_NED(self, LLA):
+        # Conversion code here. Returns a dictionary with 'north', 'east', 'down'
+        pass
 
 
 
@@ -301,6 +383,9 @@ def mavlink_monitor(mav):
 
             # Update the timestamp after each update
             drone_config.last_update_timestamp = datetime.datetime.now()
+        #update setpoint for following
+        if(drone_config.mission==2):
+            drone_config.calculate_setpoints()
 
         # Sleep for 0.5 second
         time.sleep(local_mavlink_refresh_interval)
