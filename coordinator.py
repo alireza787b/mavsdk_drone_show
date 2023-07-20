2c9961ea6dc245012e6bc61dacda562a2c09fa58
unknown <p30planets@gmail.com>
2023-07-20 04:35:20 +0000

    reverting

--- coordinator.py
+++ coordinator.py
@@ -1,6 +1,6 @@
 # -----------------------------------------------------------------------------
 # coordinator.py
-# Version: 1.0
+# Version: 0.5
 #
 # Authors: Alireza Ghaderi
 # Email: p30planets@gmail.com
@@ -40,10 +40,9 @@ import requests
 import urllib3
 import subprocess
 import navpy
-from drone_config import DroneConfig
+
 import time
 import threading
-from drone_communication import DroneCommunicator
 from local_mavlink_controller import LocalMavlinkController
 import logging
 import struct
@@ -57,9 +56,6 @@ from offboard_controller import OffboardController
 import os
 import datetime
 import logging
-import struct
-
-import math
 
 
 
@@ -113,7 +109,7 @@ default_sitl = True  # Set to True to use default 14550 port for single drone si
 online_sync_time = False  # Set to True to sync time from Internet Time Servers
 
 # Telemetry and Communication
-telem_send_interval = 1  # Send telemetry data every TELEM_SEND_INTERVAL seconds
+TELEM_SEND_INTERVAL = 1  # Send telemetry data every TELEM_SEND_INTERVAL seconds
 local_mavlink_refresh_interval = 0.1  # Refresh interval for local Mavlink connection
 broadcast_mode = True  # Set to True for broadcast mode, False for unicast mode
 
@@ -143,9 +139,225 @@ drones = {}
 
 
 
+class DroneConfig:
+    def __init__(self, hw_id=None):
+        self.hw_id = self.get_hw_id(hw_id)
+        self.trigger_time = 0
+        self.config = self.read_config()
+        self.swarm = self.read_swarm()
+        self.state = 0
+        self.pos_id = self.get_hw_id(hw_id)
+        self.mission = 0
+        self.trigger_time = 0
+        self.position = {'lat': 0, 'long': 0, 'alt': 0}
+        self.velocity = {'vel_n': 0, 'vel_e': 0, 'vel_d': 0}
+        self.yaw = 0
+        self.battery = 0
+        self.last_update_timestamp = 0
+        self.home_position = None
+        self.position_setpoint_LLA = {'lat': 0, 'long': 0, 'alt': 0}
+        self.position_setpoint_NED = {'north': 0, 'east': 0, 'down': 0}
+        self.velocity_setpoint_NED = {'north': 0, 'east': 0, 'down': 0}
+        self.yaw_setpoint=0
+        self.target_drone = None
+
+    def get_hw_id(self, hw_id=None):
+        if hw_id is not None:
+            return hw_id
+
+        hw_id_files = glob.glob("*.hwID")
+        if hw_id_files:
+            hw_id_file = hw_id_files[0]
+            print(f"Hardware ID file found: {hw_id_file}")
+            hw_id = hw_id_file.split(".")[0]
+            print(f"Hardware ID: {hw_id}")
+            return hw_id
+        else:
+            print("Hardware ID file not found. Please check your files.")
+            return None
+
+    def read_file(self, filename, source, hw_id):
+        with open(filename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['hw_id'] == hw_id:
+                    print(f"Configuration for HW_ID {hw_id} found in {source}.")
+                    return row
+        return None
+
+    def read_config(self):
+        if offline_config:
+            return self.read_file('config.csv', 'local CSV file', self.hw_id)
+        else:
+            print("Loading configuration from online source...")
+            try:
+                print(f'Attempting to download file from: {config_url}')
+                response = requests.get(config_url)
+
+                if response.status_code != 200:
+                    print(f'Error downloading file: {response.status_code} {response.reason}')
+                    return None
+
+                with open('online_config.csv', 'w') as f:
+                    f.write(response.text)
+
+                return self.read_file('online_config.csv', 'online CSV file', self.hw_id)
+
+            except Exception as e:
+                print(f"Failed to load online configuration: {e}")
+        
+        print("Configuration not found.")
+        return None
+
+    def read_swarm(self):
+        """
+        Reads the swarm configuration file, which includes the list of nodes in the swarm.
+        The function supports both online and offline modes.
+        In online mode, it downloads the swarm configuration file from the specified URL.
+        In offline mode, it reads the swarm configuration file from the local disk.
+        """
+        if offline_swarm:
+            return self.read_file('swarm.csv', 'local CSV file', self.hw_id)
+        else:
+            print("Loading swarm configuration from online source...")
+            try:
+                print(f'Attempting to download file from: {swarm_url}')
+                response = requests.get(swarm_url)
+
+                if response.status_code != 200:
+                    print(f'Error downloading file: {response.status_code} {response.reason}')
+                    return None
+
+                with open('online_swarm.csv', 'w') as f:
+                    f.write(response.text)
+
+                return self.read_file('online_swarm.csv', 'online CSV file', self.hw_id)
+
+            except Exception as e:
+                print(f"Failed to load online swarm configuration: {e}")
+        
+        print("Swarm configuration not found.")
+        return None
+    def calculate_setpoints(self):
+        self.find_target_drone()
+
+        if self.target_drone:
+            self.calculate_position_setpoint_LLA()
+            self.calculate_position_setpoint_NED()
+            self.calculate_velocity_setpoint_NED()
+            self.calculate_yaw_setpoint()
+            logging.debug(f"Setpoint updated | Position: [N:{drone_config.position_setpoint_NED.get('north')}, E:{drone_config.position_setpoint_NED.get('east')}, D:{drone_config.position_setpoint_NED.get('down')}] | Velocity: [N:{drone_config.velocity_setpoint_NED.get('vel_n')}, E:{drone_config.velocity_setpoint_NED.get('vel_e')}, D:{drone_config.velocity_setpoint_NED.get('vel_d')}] | following drone {drone_config.target_drone.hw_id}, with offsets [N:{drone_config.swarm.get('offset_n', 0)},E:{drone_config.swarm.get('offset_e', 0)},Alt:{drone_config.swarm.get('offset_alt', 0)}]")
+
+        elif self.swarm.get('follow') == 0:
+            print(f"Drone {self.hw_id} is a master drone and not following anyone.")
+        else:
+            print(f"No drone to follow for drone with hw_id: {self.hw_id}")
+
+    def find_target_drone(self):
+        # find which drone it should follow
+        follow_hw_id = int(self.swarm['follow'])
+        if follow_hw_id == 0:
+            print(f"Drone {self.hw_id} is a master drone and not following anyone.")
+        elif follow_hw_id == self.hw_id:
+            print(f"Drone {self.hw_id} is set to follow itself. This is not allowed.")
+        else:
+            self.target_drone = drones[follow_hw_id]
+            if self.target_drone:
+                print(f"Drone {self.hw_id} is following drone {self.target_drone.hw_id}")
+                pass
+            else:
+                print(f"No target drone found for drone with hw_id: {self.hw_id}")
+
+    def calculate_position_setpoint_LLA(self):
+        # find its setpoints
+        offset_n = self.swarm.get('offset_n', 0)
+        offset_e = self.swarm.get('offset_e', 0)
+        offset_alt = self.swarm.get('offset_alt', 0)
+
+        # find its target drone position
+        if self.target_drone:
+            # Calculate new LLA with offset
+            geod = Geodesic.WGS84  # define the WGS84 ellipsoid
+            g = geod.Direct(float(self.target_drone.position['lat']), float(self.target_drone.position['long']), 90, float(offset_e))
+            g = geod.Direct(g['lat2'], g['lon2'], 0, float(offset_n))
+
+            self.position_setpoint_LLA = {
+                'lat': g['lat2'],
+                'long': g['lon2'],
+                'alt': float(self.target_drone.position['alt']) + float(offset_alt),  
+            }
+
+            # The above method calculates a new LLA coordinate by moving a certain distance 
+            # in the north (latitude) and east (longitude) direction. This is an approximation, 
+            # and it assumes that a degree of latitude and longitude represents the same distance 
+            # everywhere on the globe. For small distances, this should be a reasonable approximation, 
+            # but for larger distances, this approximation may not hold true. If more accuracy is 
+            # required, one should use a more advanced method or library that can account for the 
+            # curvature of the earth.
+
+            #print(f"Position setpoint for drone {self.hw_id}: {self.position_setpoint_LLA}")
+        else:
+            print(f"No target drone found for drone with hw_id: {self.hw_id}")
+
+    def calculate_position_setpoint_NED(self):
+        if self.target_drone:
+            self.position_setpoint_NED = self.convert_LLA_to_NED(self.position_setpoint_LLA)
+            #print(f"NED Position setpoint for drone {self.hw_id}: {self.position_setpoint_NED}")
+        else:
+            print(f"No target drone found for drone with hw_id: {self.hw_id}")
+            
+    def calculate_yaw_setpoint(self):
+        if self.target_drone:
+            self.yaw_setpoint = self.target_drone.yaw
+            #print(f"Yaw setpoint for drone {self.hw_id}: {self.yaw_setpoint}")
+        else:
+            print(f"No target drone found for drone with hw_id: {self.hw_id}")
+
+
+    def calculate_velocity_setpoint_NED(self):
+        # velocity setpoints is exactly the same as the target drone velocity
+        if self.target_drone:
+            self.velocity_setpoint_NED = self.target_drone.velocity
+            #print(f"NED Velocity setpoint for drone {self.hw_id}: {self.velocity_setpoint_NED}")
+        else:
+            print(f"No target drone found for drone with hw_id: {self.hw_id}")
+
+    def convert_LLA_to_NED(self, LLA):
+        if self.home_position:
+            lat = LLA['lat']
+            long = LLA['long']
+            alt = LLA['alt']
+            home_lat = self.home_position['lat']
+            home_long = self.home_position['long']
+            home_alt = self.home_position['alt']
+
+            ned = navpy.lla2ned(lat, long, alt, home_lat, home_long, home_alt)
+
+            position_NED = {
+                'north': ned[0],
+                'east': ned[1],
+                'down': ned[2]
+            }
+
+            return position_NED
+        else:
+            print("Home position is not set")
+            return None
+        
+    def radian_to_degrees_heading(self,yaw_radians):
+        # Convert the yaw angle to degrees
+        yaw_degrees = math.degrees(yaw_radians)
+
+        # Normalize to a heading (0-360 degrees)
+        if yaw_degrees < 0:
+            yaw_degrees += 360
+
+        return yaw_degrees
+
+
 
 # Initialize DroneConfig
-drone_config = DroneConfig(drones=drones,config_url=config_url,swarm_url=swarm_url)
+drone_config = DroneConfig()
 
 
 
@@ -246,6 +458,9 @@ def stop_mavlink_routing(mavlink_router_process):
 local_drone_controller = LocalMavlinkController(drone_config, local_mavlink_port, local_mavlink_refresh_interval)
 
 
+import struct
+
+import math
 
 
 
@@ -265,6 +480,7 @@ def get_nodes():
     return get_nodes.nodes
 
    
+
 
 # Helper functions
 def set_drone_config(hw_id, pos_id, state, mission, trigger_time, position, velocity, yaw, battery, last_update_timestamp):
@@ -348,6 +564,85 @@ def get_drone_state():
 
     return drone_state
 
+
+
+def send_drone_state():
+    """
+    Sends the drone state over UDP to the GCS and optionally to other drones in the swarm.
+
+    The state information includes hardware id, position id, current state, 
+    trigger time, position, velocity, battery voltage, and follow mode.
+    
+    Each state variable is packed into a binary packet and sent every `TELEM_SEND_INTERVAL` seconds.
+    If `broadcast_mode` is True, the state is also sent to all other drones in the swarm.
+
+    The structure of the packet is as follows:
+    - Start of packet (uint8)
+    - Hardware ID (uint16)
+    - Position ID (uint16)
+    - State (uint8)
+    - Trigger Time (uint32)
+    - Latitude, Longitude, Altitude (double)
+    - North, East, Down velocities (double)
+    - yaw (double)
+    - Battery Voltage (double)
+    - Follow Mode (uint8)
+    - End of packet (uint8)
+    """
+    udp_ip = drone_config.config['gcs_ip']  # IP address of the ground station
+    udp_port = int(drone_config.config['debug_port'])  # UDP port to send telemetry data to
+
+    while True:
+        drone_state = get_drone_state()
+
+        # Create a struct format string based on the data types
+        telem_struct_fmt = '=BHHBBIddddddddBB'  # update this to match your data types
+        # H is for uint16
+        # B is for uint8
+        # I is for uint32
+        # d is for double (float64)
+        # Pack the telemetry data into a binary packet
+        #print(drone_state)
+        packet = struct.pack(telem_struct_fmt,
+                             77,  # start of packet
+                             drone_state['hw_id'],
+                             drone_state['pos_id'],
+                             drone_state['state'],
+                             drone_state['mission'],
+                             drone_state['trigger_time'],
+                             drone_state['position_lat'],
+                             drone_state['position_long'],
+                             drone_state['position_alt'],
+                             drone_state['velocity_north'],
+                             drone_state['velocity_east'],
+                             drone_state['velocity_down'],
+                             drone_state['yaw'],
+                             drone_state['battery_voltage'],
+                             drone_state['follow_mode'],
+                             88)  # end of packet
+        telem_packet_size = len(packet)
+        # If broadcast_mode is True, send to all nodes
+        if broadcast_mode:
+            nodes = get_nodes()
+            # Send to all other nodes
+            for node in nodes:
+                if int(node["hw_id"]) != drone_state['hw_id']:
+                    send_packet_to_node(packet, node["ip"], int(node["debug_port"]))
+                    #print(f'Sent telemetry {telem_packet_size} Bytes to drone {int(node["hw_id"])} with IP: {node["ip"]} ')
+
+
+        # Always send to GCS
+        send_packet_to_node(packet, udp_ip, udp_port)
+
+        #print(f"Sent telemetry data to GCS: {packet}")
+        #print(f"Sent telemetry {telem_packet_size} Bytes to GCS")
+        #print(f"Values: hw_id: {drone_state['hw_id']}, state: {drone_state['state']}, Mission: {drone_state['mission']}, Latitude: {drone_state['position_lat']}, Longitude: {drone_state['position_long']}, Altitude : {drone_state['position_alt']}, follow_mode: {drone_state['follow_mode']}, trigger_time: {drone_state['trigger_time']}")
+        current_time = int(time.time())
+        #print(f"Current system time: {current_time}")
+        
+        # Update the global variable to keep track of the packet size
+
+        time.sleep(TELEM_SEND_INTERVAL)  # send telemetry data every TELEM_SEND_INTERVAL seconds
 
 
 
@@ -442,11 +737,8 @@ current_time = now.strftime("%Y-%m-%d_%H-%M-%S")
 # Set up logging
 log_filename = os.path.join('logs', f'{current_time}.log')
 logging.basicConfig(filename=log_filename, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-#setting up droneComm
-drone_comms = DroneCommunicator(drone_config, telem_send_interval, income_packet_check_interval, broadcast_mode, command_packet_size, telem_packet_size, command_struct_fmt, telem_struct_fmt)
-drone_comms.start_threads()
-
+telemetry_thread = threading.Thread(target=send_drone_state)
+command_thread = threading.Thread(target=read_packets)
 
 
 # Main function
@@ -462,7 +754,14 @@ def main():
         print("Initializing MAVLink...")
         mavlink_router_process = initialize_mavlink()
         time.sleep(2)
+        # Start the telemetry thread
+        print("Starting telemetry thread...")
+        telemetry_thread.start()
+
+        # Start the command reading thread
+        print("Starting command reading thread...")
        
+        command_thread.start()
 
         # Enter a loop where the application will continue running
         while True:
