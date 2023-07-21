ae65cca1d1a1371ada566eb28c13a5892f6ed35f
unknown <p30planets@gmail.com>
2023-07-21 08:31:36 +0000

    changing calc setpoitns

--- src/drone_config.py
+++ src/drone_config.py
@@ -184,6 +184,53 @@ class DroneConfig:
         
         print("Swarm configuration not found.")
         return None
+    
+    def find_target_drone(self):
+        # find which drone it should follow
+        follow_hw_id = int(self.swarm['follow'])
+        if follow_hw_id == 0:
+            print(f"Drone {self.hw_id} is a master drone and not following anyone.")
+        elif follow_hw_id == self.hw_id:
+            print(f"Drone {self.hw_id} is set to follow itself. This is not allowed.")
+        else:
+            self.target_drone = self.drones[follow_hw_id]
+            if self.target_drone:
+                print(f"Drone {self.hw_id} is following drone {self.target_drone.hw_id}")
+                pass
+            else:
+                print(f"No target drone found for drone with hw_id: {self.hw_id}")
+
+    def calculate_position_setpoint_LLA(self):
+        # find its setpoints
+        offset_n = self.swarm.get('offset_n', 0)
+        offset_e = self.swarm.get('offset_e', 0)
+        offset_alt = self.swarm.get('offset_alt', 0)
+
+        # find its target drone position
+        if self.target_drone:
+            # Calculate new LLA with offset
+            geod = Geodesic.WGS84  # define the WGS84 ellipsoid
+            g = geod.Direct(float(self.target_drone.position['lat']), float(self.target_drone.position['long']), 90, float(offset_e))
+            g = geod.Direct(g['lat2'], g['lon2'], 0, float(offset_n))
+
+            self.position_setpoint_LLA = {
+                'lat': g['lat2'],
+                'long': g['lon2'],
+                'alt': float(self.target_drone.position['alt']) + float(offset_alt),  
+            }
+
+            # The above method calculates a new LLA coordinate by moving a certain distance 
+            # in the north (latitude) and east (longitude) direction. This is an approximation, 
+            # and it assumes that a degree of latitude and longitude represents the same distance 
+            # everywhere on the globe. For small distances, this should be a reasonable approximation, 
+            # but for larger distances, this approximation may not hold true. If more accuracy is 
+            # required, one should use a more advanced method or library that can account for the 
+            # curvature of the earth.
+
+            #print(f"Position setpoint for drone {self.hw_id}: {self.position_setpoint_LLA}")
+        else:
+            print(f"No target drone found for drone with hw_id: {self.hw_id}")
+    
     def calculate_setpoints(self):
         self.find_target_drone()
 
