cf44c3d2929f5691869dcdcfdaef508ce01f794a
unknown <p30planets@gmail.com>
2023-07-21 06:03:22 +0000

    optimizing droneComm receiving Commands

--- coordinator.py
+++ coordinator.py
@@ -272,6 +272,9 @@ def main():
             # Get the drone state
             #drone_state = get_drone_state()
 
+            if drone_config.mission == 2 and drone_config.state != 0 and int(drone_config.swarm.get('follow')) != 0:
+                drone_config.calculate_setpoints()
+            
             # Schedule the drone mission if the trigger time has been reached
             schedule_mission()
 
