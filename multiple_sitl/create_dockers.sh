e68abca98b5dd2136929a7df5cec37b4a0f31730
unknown <p30planets@gmail.com>
2023-07-23 06:55:09 +0000

    adding startup.sh amd dpcker

---
+++ multiple_sitl/create_dockers.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Get the number of instances as input
+num_instances=$1
+
+# Check if the number of instances is provided
+if [ -z "$num_instances" ]
+then
+    echo "Please provide the number of instances as argument"
+    exit 1
+fi
+
+# Loop to create instances
+for (( i=1; i<=$num_instances; i++ ))
+do
+    echo "Creating instance drone-$i"
+
+    # Create an empty .hwID file in your local directory
+    touch $i.hwID
+
+    # Copy the .hwID file to the Docker container
+    docker cp $i.hwID drone-$i:/root/mavsdk_drone_show/
+
+    # Run the Docker container
+    docker run --name drone-$i -d drone-xv2 bash /root/mavsdk_drone_show/multiple_sitl/startup.sh
+
+    # Remove the local .hwID file
+    rm $i.hwID
+done
