24f6196a770fc1ff5825aa632911780d7b2f5763
unknown <p30planets@gmail.com>
2023-07-23 07:12:07 +0000

    new image for docker

--- multiple_sitl/startup.sh
+++ multiple_sitl/startup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+
+cd ~/PX4-Autopilot
+HEADLESS=1 make px4_sitl gazebo &
+
 cd ~/mavsdk_drone_show
 git pull
 python3 ~/mavsdk_drone_show/coordinator.py &
 
-cd ~/PX4-Autopilot
-HEADLESS=1 make px4_sitl gazebo &
 
 tail -f /dev/null
