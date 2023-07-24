5c807fe3bc838adc21a9b5e703ba47f51b83c804
unknown <p30planets@gmail.com>
2023-07-24 04:21:22 +0000

    set_sys_id

--- multiple_sitl/set_sys_id.py
+++ multiple_sitl/set_sys_id.py
@@ -1,7 +1,7 @@
 import os
 
 # Directory where the .hwID files are
-hwid_dir = '/root/mavsdk_drone_show'
+hwid_dir = '~/mavsdk_drone_show'
 
 # The rcS file path
 rcs_file_path = '~/PX4-Autopilot/build/px4_sitl_default/etc/init.d-posix/rcS'
@@ -20,6 +20,8 @@ if not hwid_file:
 with open(os.path.join(hwid_dir, hwid_file), 'r') as f:
     hwid = f.read().strip()
 
+print(f"Found .hwID file with ID: {hwid}")
+
 # Read the rcS file
 with open(rcs_file_path, 'r') as f:
     rcs_content = f.readlines()
@@ -27,9 +29,14 @@ with open(rcs_file_path, 'r') as f:
 # Find the line where MAV_SYS_ID is set
 index = next(i for i, line in enumerate(rcs_content) if 'param set MAV_SYS_ID' in line)
 
-# Add a new line after the found line
-rcs_content.insert(index + 1, f'param set MAV_SYS_ID {hwid}\n')
+# Check if the line 'param set MAV_SYS_ID hwID' already exists
+if any(f'param set MAV_SYS_ID {hwid}' in line for line in rcs_content):
+    print(f"param set MAV_SYS_ID {hwid} is already in the file.")
+else:
+    print(f"Adding line: param set MAV_SYS_ID {hwid}")
+    # Add a new line after the found line
+    rcs_content.insert(index + 1, f'param set MAV_SYS_ID {hwid}\n')
 
-# Write the rcS file back
-with open(rcs_file_path, 'w') as f:
-    f.writelines(rcs_content)
+    # Write the rcS file back
+    with open(rcs_file_path, 'w') as f:
+        f.writelines(rcs_content)
