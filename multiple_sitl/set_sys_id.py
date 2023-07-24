2c04a6d96739d37bf09bc9ccedadd8c2aab50258
unknown <p30planets@gmail.com>
2023-07-24 04:32:27 +0000

    set_sys_id

--- multiple_sitl/set_sys_id.py
+++ multiple_sitl/set_sys_id.py
@@ -1,13 +1,11 @@
 import os
+import re
 
 # Directory where the .hwID files are
-hwid_dir = '~/mavsdk_drone_show'
+hwid_dir = os.path.expanduser('~/mavsdk_drone_show')
 
 # The rcS file path
-rcs_file_path = '~/PX4-Autopilot/build/px4_sitl_default/etc/init.d-posix/rcS'
-
-# Expand the rcS file path
-rcs_file_path = os.path.expanduser(rcs_file_path)
+rcs_file_path = os.path.expanduser('~/PX4-Autopilot/build/px4_sitl_default/etc/init.d-posix/rcS')
 
 # Find the .hwID file
 hwid_file = next((f for f in os.listdir(hwid_dir) if f.endswith('.hwID')), None)
@@ -16,7 +14,7 @@ hwid_file = next((f for f in os.listdir(hwid_dir) if f.endswith('.hwID')), None)
 if not hwid_file:
     raise FileNotFoundError('.hwID file not found')
 
-# Read the hwID from the file
+# Read the hwid from the file
 with open(os.path.join(hwid_dir, hwid_file), 'r') as f:
     hwid = f.read().strip()
 
@@ -26,17 +24,17 @@ print(f"Found .hwID file with ID: {hwid}")
 with open(rcs_file_path, 'r') as f:
     rcs_content = f.readlines()
 
-# Find the line where MAV_SYS_ID is set
-index = next(i for i, line in enumerate(rcs_content) if 'param set MAV_SYS_ID' in line)
+# Find any existing MAV_SYS_ID line(s) and remove them if not equal to the correct hwid
+rcs_content = [line for line in rcs_content if not (re.match('param set MAV_SYS_ID \d+', line) and not line.strip().endswith(hwid))]
 
-# Check if the line 'param set MAV_SYS_ID hwID' already exists
+# Check if the correct line already exists
 if any(f'param set MAV_SYS_ID {hwid}' in line for line in rcs_content):
     print(f"param set MAV_SYS_ID {hwid} is already in the file.")
 else:
     print(f"Adding line: param set MAV_SYS_ID {hwid}")
-    # Add a new line after the found line
-    rcs_content.insert(index + 1, f'param set MAV_SYS_ID {hwid}\n')
+    # Add the correct line at the end of the file
+    rcs_content.append(f'param set MAV_SYS_ID {hwid}\n')
 
-    # Write the rcS file back
-    with open(rcs_file_path, 'w') as f:
-        f.writelines(rcs_content)
+# Write the rcS file back
+with open(rcs_file_path, 'w') as f:
+    f.writelines(rcs_content)
