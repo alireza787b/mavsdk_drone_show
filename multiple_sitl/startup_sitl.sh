3daba4dc71fb1ff2e1de8b592061712a1cb57cf7
unknown <p30planets@gmail.com>
2023-08-07 07:55:04 +0000

    changed handling the home dirctory works with vmware as well

--- multiple_sitl/startup_sitl.sh
+++ multiple_sitl/startup_sitl.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-# Function to handle SIGINT
-cleanup() {
-  echo "Received interrupt, terminating background processes..."
-  kill $gazebo_pid $coordinator_pid
-  exit 0
-}
-
-# Trap SIGINT
-trap 'cleanup' INT
-
-
-# Determine the directory path
-DIRECTORY=$HOME/mavsdk_drone_show
-
-# Echo the directory being searched
-echo "Looking for .hwID file in $DIRECTORY..."
-
 # Wait for the .hwID file to exist
-while [ ! -f "$DIRECTORY/*.hwID" ]
+while [ ! -f ~/mavsdk_drone_show/*.hwID ]
 do
   echo "Waiting for hwID file..."
   sleep 1
@@ -26,25 +9,29 @@ done
 
 # Once the .hwID file exists, continue with the rest of the script
 echo "Found .hwID file, continuing with the script."
-cd $DIRECTORY
+cd ~/mavsdk_drone_show
 echo "Stashing and pulling the latest changes from the repository..."
 git stash
 git pull
 
-echo "Checking Python Requirements..."
-pip install -r requirements.txt
+
+echo "Cheking Python Requirements..."
+pip install -r  requirements.txt
 
 echo "Running the set_sys_id.py script to set the MAV_SYS_ID..."
-python3 $DIRECTORY/multiple_sitl/set_sys_id.py
+python3 ~/mavsdk_drone_show/multiple_sitl/set_sys_id.py
 
 # Start the px4_sitl gazebo process in the background
-cd $HOME/PX4-Autopilot
+echo "Starting the px4_sitl gazebo process..."
+cd ~/PX4-Autopilot
+hwid_file=$(find ~/mavsdk_drone_show -name '*.hwID')
+hwid=$(echo $hwid_file | cut -d'.' -f2)
+export px4_instance=$hwid-1
 HEADLESS=1 make px4_sitl gazebo &
-gazebo_pid=$!
 
 # Start the coordinator.py process in the background
-cd $HOME/mavsdk_drone_show
+echo "Starting the coordinator.py process..."
+cd ~/mavsdk_drone_show
 python3 ~/mavsdk_drone_show/coordinator.py &
-coordinator_pid=$!
 
 tail -f /dev/null
