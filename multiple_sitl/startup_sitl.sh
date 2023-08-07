a445a6be08ccce42d9ad54ae8c58a675626223c8
unknown <p30planets@gmail.com>
2023-08-07 07:37:43 +0000

    changed handling the home dirctory works with vmware as well

--- multiple_sitl/startup_sitl.sh
+++ multiple_sitl/startup_sitl.sh
@@ -1,37 +1,43 @@
 #!/bin/bash
 
+# Determine the location of the mavsdk_drone_show directory
+if [ -d "/root/mavsdk_drone_show" ]; then
+  DIRECTORY="/root/mavsdk_drone_show"
+  echo "Found directory at $DIRECTORY"
+else
+  USER_HOME=$(eval echo ~$USER)
+  DIRECTORY="$USER_HOME/mavsdk_drone_show"
+  echo "Found directory at $DIRECTORY"
+fi
+
 # Wait for the .hwID file to exist
-while [ ! -f ~/mavsdk_drone_show/*.hwID ]
+while [ ! -f $DIRECTORY/*.hwID ]
 do
-  echo "Waiting for hwID file..."
+  echo "Waiting for hwID file in $DIRECTORY..."
   sleep 1
 done
 
-# Once the .hwID file exists, continue with the rest of the script
 echo "Found .hwID file, continuing with the script."
-cd ~/mavsdk_drone_show
+cd $DIRECTORY
 echo "Stashing and pulling the latest changes from the repository..."
 git stash
 git pull
 
-
-echo "Cheking Python Requirements..."
-pip install -r  requirements.txt
+echo "Checking Python Requirements..."
+pip install -r requirements.txt
 
 echo "Running the set_sys_id.py script to set the MAV_SYS_ID..."
-python3 ~/mavsdk_drone_show/multiple_sitl/set_sys_id.py
+python3 $DIRECTORY/multiple_sitl/set_sys_id.py
 
-# Start the px4_sitl gazebo process in the background
 echo "Starting the px4_sitl gazebo process..."
 cd ~/PX4-Autopilot
-hwid_file=$(find ~/mavsdk_drone_show -name '*.hwID')
+hwid_file=$(find $DIRECTORY -name '*.hwID')
 hwid=$(echo $hwid_file | cut -d'.' -f2)
-export px4_instance=$hwid-1
+export px4_instance=$((hwid-1))
 HEADLESS=1 make px4_sitl gazebo &
 
-# Start the coordinator.py process in the background
 echo "Starting the coordinator.py process..."
-cd ~/mavsdk_drone_show
-python3 ~/mavsdk_drone_show/coordinator.py &
+cd $DIRECTORY
+python3 $DIRECTORY/coordinator.py &
 
 tail -f /dev/null
