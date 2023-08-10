48100bceb313c1c2f189288d9db0de11c5573cb9
unknown <p30planets@gmail.com>
2023-08-10 16:48:40 +0000

    changed sitl to suport jamvsim

--- multiple_sitl/startup_sitl.sh
+++ multiple_sitl/startup_sitl.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
 
-# To run the script with graphics enabled, you would execute the script with the g argument, like this:
+# To run the script with gazebo classic graphics enabled, you would execute the script with the g argument, like this:
 # ./startup.sh g
 
-# To run without graphics, you would simply execute the script without any arguments:
+# To run without graphics (headless), you can either provide the h argument or no arguments:
+# ./startup.sh h
+# OR simply
 # ./startup.sh
+
+# To run with jmavsim, you would execute the script with the j argument:
+# ./startup.sh j
 
 # Function to handle SIGINT
 cleanup() {
   echo "Received interrupt, terminating background processes..."
-  kill $gazebo_pid
+  kill $simulation_pid
   exit 0
 }
 
 # Trap SIGINT
 trap 'cleanup' INT
 
-# Check for the 'g' argument to enable or disable graphics
-if [[ $1 == "g" ]]; then
-  GRAPHICS_COMMAND="make px4_sitl gazebo"
-  echo "Graphics enabled."
-else
-  GRAPHICS_COMMAND="HEADLESS=1 make px4_sitl gazebo"
-  echo "Graphics disabled."
-fi
+# Determine the command based on the provided argument
+case $1 in
+  g)
+    SIMULATION_COMMAND="make px4_sitl gazebo"
+    echo "Graphics enabled."
+    ;;
+  j)
+    SIMULATION_COMMAND="make px4_sitl jmavsim"
+    echo "Using jmavsim."
+    ;;
+  h|*)
+    SIMULATION_COMMAND="HEADLESS=1 make px4_sitl gazebo"
+    echo "Graphics disabled."
+    ;;
+esac
 
 # Wait for the .hwID file to exist
 while [ ! -f ~/mavsdk_drone_show/*.hwID ]
@@ -44,12 +56,12 @@ pip install -r requirements.txt
 echo "Running the set_sys_id.py script to set the MAV_SYS_ID..."
 python3 ~/mavsdk_drone_show/multiple_sitl/set_sys_id.py
 
-echo "Starting the px4_sitl gazebo process in a new terminal window..."
-gnome-terminal -- bash -c "cd ~/PX4-Autopilot; hwid_file=\$(find ~/mavsdk_drone_show -name '*.hwID'); hwid=\$(echo \$hwid_file | cut -d'.' -f2); export px4_instance=\$hwid-1; $GRAPHICS_COMMAND; bash" &
-gazebo_pid=$!
+echo "Starting the simulation process in a new terminal window..."
+gnome-terminal -- bash -c "cd ~/PX4-Autopilot; hwid_file=\$(find ~/mavsdk_drone_show -name '*.hwID'); hwid=\$(echo \$hwid_file | cut -d'.' -f2); export px4_instance=\$hwid-1; $SIMULATION_COMMAND; bash" &
+simulation_pid=$!
 
 echo "Starting the coordinator.py process in another new terminal window..."
 gnome-terminal -- bash -c "python3 ~/mavsdk_drone_show/coordinator.py; bash" &
 
-echo "Press Ctrl+C to stop the gazebo process."
-wait $gazebo_pid
+echo "Press Ctrl+C to stop the simulation process."
+wait $simulation_pid
