d96087d11972dc71048b1be7e78cc528efbbce7f
unknown <p30planets@gmail.com>
2023-08-18 01:47:53 +0000

    working grap GUI

---
+++ app/dashboard/drone-dashboard/src/components/DroneGraph.js
@@ -0,0 +1,135 @@
+import React, { useRef, useEffect } from 'react';
+import CytoscapeComponent from 'react-cytoscapejs';
+import '../styles/DroneGraph.css';
+
+
+
+
+function DroneGraph({ swarmData, onSelectDrone }) {
+    const cyRef = useRef(null);
+    
+    const transformToGraphData = (swarmData) => {
+        const nodes = swarmData.map(drone => {
+            let role = 'Follower';
+            if (drone.follow === '0') {
+                role = 'Top Leader';
+            } else if (swarmData.some(d => d.follow === drone.hw_id)) {
+                role = 'Intermediate Leader';
+            }
+            return {
+                data: { id: drone.hw_id, label: drone.hw_id, role: role, ...drone }
+            };
+        });
+        const edges = swarmData
+            .filter(drone => drone.follow !== '0')
+            .map(drone => ({
+                data: { source: drone.hw_id, target: drone.follow }
+            }));
+        return [...nodes, ...edges];
+    };
+
+    const elements = transformToGraphData(swarmData);
+    const prevElementsRef = useRef(swarmData);
+
+    useEffect(() => {
+        if (cyRef.current) {
+            // Update elements in the graph
+            cyRef.current.batch(() => {
+                cyRef.current.remove(cyRef.current.elements());
+                cyRef.current.add(elements);
+            });
+    
+            // Run the layout and fit the graph
+            cyRef.current.layout(coseLayout).run();
+            cyRef.current.fit();
+    
+            // Add a resize listener
+            cyRef.current.on('resize', () => {
+                cyRef.current.layout(coseLayout).run();
+            });
+    
+            // Node click listener
+            cyRef.current.on('tap', 'node', function (evt) {
+                const clickedNodeId = evt.target.id();
+                onSelectDrone(clickedNodeId);
+            });
+    
+            // Cleanup on unmount
+            return () => {
+                if (cyRef.current) {
+                    cyRef.current.removeListener('tap', 'node');
+                    cyRef.current.removeListener('resize');
+                }
+            };
+        }
+    }, [swarmData, onSelectDrone]);
+
+    const coseLayout = {
+        name: 'cose',
+        directed: true,
+        padding: 10,
+        fit: true,
+    };
+
+    const style = {
+        width: '100%',
+        height: '100vh',
+    };
+
+    const styles = [
+        {
+            selector: 'node',
+            style: {
+                'label': 'data(hw_id)',
+                'text-valign': 'center', 
+                'text-halign': 'center',
+                color: 'white'
+            }
+        },
+        {
+            selector: 'node[role="Top Leader"]',
+            style: {
+                'background-color': '#28a745'  // Green for Top Leader
+            }
+        },
+        {
+            selector: 'node[role="Intermediate Leader"]',
+            style: {
+                'background-color': '#ffcc00'  // Yellow for Intermediate Leader
+            }
+        },
+        {
+            selector: 'node[role="Follower"]',
+            style: {
+                'background-color': '#007bff'  // Blue for Follower
+            }
+        },
+        {
+            selector: 'edge',
+            style: {
+                'curve-style': 'bezier',
+                'target-arrow-shape': 'triangle'
+            }
+        },
+        {
+    selector: 'node:selected',
+    style: {
+        'border-width': '4px',    // Add a border to the selected node
+        'border-color': '#ff5733' // Color of the border (you can adjust this as desired)
+    }
+}
+
+    ];
+    
+    return (
+        <CytoscapeComponent 
+            cy={(cy) => { cyRef.current = cy; }}
+            elements={elements} 
+            style={style}
+            stylesheet={styles}
+            layout={coseLayout} 
+        />
+    );
+}
+
+export default DroneGraph;
--- app/dashboard/drone-dashboard/src/components/SwarmDesign.js
+++ app/dashboard/drone-dashboard/src/components/SwarmDesign.js
@@ -1,42 +1,125 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Papa from 'papaparse';
+import '../styles/SwarmDesign.css';
+import DroneGraph from './DroneGraph';
+
+const transformToGraphData = (swarmData) => {
+    const nodes = swarmData.map(drone => ({
+        data: { id: drone.hw_id, label: drone.hw_id, ...drone } // Set label to hw_id
+    }));
+
+    const edges = swarmData
+        .filter(drone => drone.follow !== '0')
+        .map(drone => ({
+            data: { source: drone.hw_id, target: drone.follow }
+        }));
+
+    return [...nodes, ...edges];
+};
+
+function categorizeDrones(swarmData) {
+    const topLeaders = swarmData.filter(drone => drone.follow === '0');
+    const topLeaderIdsSet = new Set(topLeaders.map(leader => leader.hw_id));
+
+    // Count followers for each drone
+    const followerCounts = {};
+    swarmData.forEach(drone => {
+        if (!followerCounts[drone.follow]) {
+            followerCounts[drone.follow] = 0;
+        }
+        followerCounts[drone.follow]++;
+    });
+
+    // Identify intermediate leaders
+    const intermediateLeaders = swarmData.filter(drone => 
+        !topLeaderIdsSet.has(drone.hw_id) && followerCounts[drone.hw_id]
+    );
+    const intermediateLeaderIdsSet = new Set(intermediateLeaders.map(drone => drone.hw_id));
+    
+    return {
+        topLeaders,
+        intermediateLeaders,
+        topLeaderIdsSet,
+        intermediateLeaderIdsSet
+    };
+}
+
+function isEqual(arr1, arr2) {
+    return JSON.stringify(arr1) === JSON.stringify(arr2);
+}
+
 
 
 function SwarmDesign() {
     const [swarmData, setSwarmData] = useState([]);
     const [configData, setConfigData] = useState([]);
+    const { topLeaders, intermediateLeaders } = categorizeDrones(swarmData);
+    const [selectedDroneId, setSelectedDroneId] = useState(null); // 1. Added state for selected drone ID
+    
+    const handleDroneCardClick = (droneId) => {
+        setSelectedDroneId(droneId);
+    };
 
     useEffect(() => {
-        // Fetch and parse swarm.csv
-        axios.get('http://localhost:5000/get-swarm-data')
-      .then(response => {
-          setSwarmData(response.data);
-      })
-      .catch(error => {
-          console.error("Error fetching swarm data:", error);
-      });
+        // Fetch swarm data
+        const fetchSwarmData = axios.get('http://localhost:5000/get-swarm-data');
+        // Fetch config data
+        const fetchConfigData = axios.get('http://localhost:5000/get-config-data');
+    
+        Promise.all([fetchSwarmData, fetchConfigData])
+            .then(([swarmResponse, configResponse]) => {
+                setSwarmData(swarmResponse.data);
+                setConfigData(configResponse.data);
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+            });
+    
+    }, []); // Empty dependency array ensures this runs once on component mount
+    
 
-
-      // Fetch and parse config.csv
-      axios.get('http://localhost:5000/get-config-data')
-      .then(response => {
-          setConfigData(response.data);
-      })
-      .catch(error => {
-          console.error("Error fetching config data:", error);
-      });
-
-
-
-
-          
-    }, []);
+    const dronesFollowing = (leaderId) => {
+        return swarmData.filter(drone => drone.follow === leaderId).map(drone => drone.hw_id);
+    };
 
     return (
-        <div>
-            {/* Your 3D and 2D visualization logic will go here */}
+
+        <div className="swarm-design-container">
+
+        <div className="swarm-container">
+        
+        {swarmData.length ? swarmData.map(drone => (
+                    <div 
+                        className={`swarm-drone-card ${drone.hw_id === selectedDroneId ? 'selected-drone' : ''} ${topLeaders.includes(drone) ? 'top-leader' : intermediateLeaders.includes(drone) ? 'intermediate-leader' : 'follower'}`} 
+                        key={drone.hw_id}
+                        onClick={() => handleDroneCardClick(drone.hw_id)} // Added onClick handler
+                    >
+                    <h3>Drone ID: {drone.hw_id}</h3>
+                    <p>
+                        {drone.follow === '0' ? 
+                            <span className="role leader">Top Leader</span> : 
+                            intermediateLeaders.includes(drone) ? 
+                            <span className="role intermediate">Intermediate Leader (Follows Drone {drone.follow})</span> :
+                            <span className="role follower">Follows Drone {drone.follow}</span>
+                        }
+                    </p>
+                    {(topLeaders.includes(drone) || intermediateLeaders.includes(drone)) && (
+                        <p>Followed by: {dronesFollowing(drone.hw_id).join(', ')}</p>
+                    )}
+                    <div className="collapsible-details">
+                        <p><i className="position-icon"></i> Position Offset (m): North: {drone.offset_n}, East: {drone.offset_e}</p>
+                        <p><i className="ip-icon"></i> Altitude Offset: {drone.offset_alt}</p>
+                        {/* Add other properties as needed */}
+                    </div>
+                </div>
+            )) : <p>No data available for swarm configuration.</p>}
         </div>
+        <div className="swarm-graph-container">
+        <DroneGraph swarmData={swarmData} onSelectDrone={setSelectedDroneId} />
+
+        </div>
+    </div>
     );
 }
 
