8ac8dde244ef78ad05b3ba5d079207d49084245d
unknown <p30planets@gmail.com>
2023-09-19 05:52:12 +0000

    foreward fromllinode to main PC

--- tools/mavlink-router-install.sh
+++ tools/mavlink-router-install.sh
@@ -5,12 +5,13 @@
 # Email: p30planets@gmail.com
 # Introduction: This script installs mavlink-router as part of the MAVSDK-Drone-Show project.
 
+
 echo "================================================================="
-echo "MAVSDK-Drone-Show Installation Script"
+echo "MAVSDK-Drone-Show Mavlink-router Installation Script"
 echo "Author: Alireza Ghaderi"
-echo "GitHub: alireza787b/mavsdk_drone_show"
+echo "GitHub: https://github.com/alireza787b/mavsdk_drone_show"
 echo "Contact: p30planets@gmail.com"
-echo "For more information, visit the GitHub profile: alireza787b/mavsdk_drone_show"
+echo "For more information, visit the GitHub Repo"
 echo "================================================================="
 
 # Navigate to home directory
@@ -19,53 +20,38 @@ cd ~
 # Check if mavlink-router is already installed
 if command -v mavlink-routerd &> /dev/null; then
     echo "mavlink-router is already installed. You're good to go!"
+    echo "use this command:"
+    echo "mavlink-routerd -e 100.100.184.90:14550 0.0.0.0:14550"   
     exit 0
 fi
 
-# Step 1: Update package lists
-echo "Updating package lists..."
-sudo apt update
+# If the mavlink-router directory exists, remove it
+if [ -d "mavlink-router" ]; then
+    echo "Removing existing mavlink-router directory..."
+    rm -rf mavlink-router
+fi
 
-# Step 2: Install required packages
-echo "Installing required packages..."
-sudo apt install -y git autoconf libtool python3 python3-future python3-lxml g++
+# Update and install packages
+sudo apt update && sudo apt install -y git autoconf libtool python3 python3-future python3-lxml g++ || { echo "Installation of packages failed"; exit 1; }
 
-# Step 3: Clone mavlink-router repository
-echo "Cloning mavlink-router repository..."
-git clone https://github.com/mavlink-router/mavlink-router.git
-
-# Step 4: Change directory to mavlink-router
-echo "Changing directory to mavlink-router..."
+# Clone and navigate into the repository
+git clone https://github.com/mavlink-router/mavlink-router.git || { echo "Cloning of repository failed"; exit 1; }
 cd mavlink-router
 
-# Step 5: Initialize git submodules
-echo "Initializing git submodules..."
-git submodule update --init --recursive
-
-# Step 6: Run autogen.sh
-echo "Running autogen.sh..."
-./autogen.sh
-
-# Step 7: Configure the build system
-echo "Configuring the build system..."
+# Build and install
+git submodule update --init --recursive && \
+./autogen.sh && \
 ./configure CFLAGS='-g -O2' \
             --sysconfdir=/etc --localstatedir=/var --libdir=/usr/lib64 \
-            --prefix=/usr
+            --prefix=/usr && \
+make && \
+sudo make install || { echo "Build or installation failed"; exit 1; }
 
-# Step 8: Compile mavlink-router
-echo "Compiling mavlink-router..."
-make
-
-# Step 9: Install mavlink-router
-echo "Installing mavlink-router..."
-sudo make install
-
-# Step 10: Navigate back to the home directory
+# Navigate back to home directory
 cd ~
 
-# Step 11: Print completion message
+# Print success message
 echo "mavlink-router installed successfully. You're good to go!"
-
 echo "use this command:"
 echo "mavlink-routerd -e 100.100.184.90:14550 0.0.0.0:14550"
 
