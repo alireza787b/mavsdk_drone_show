44cbf269a258c10edfb452f07487ade78cd6713e
Alireza Ghaderi <p30planets@gmail.com>
2024-04-25 05:48:51 +0000

    Update create_dockers.sh for branch info

--- multiple_sitl/create_dockers.sh
+++ multiple_sitl/create_dockers.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
 
+# *****************************************************************************************
+# Docker Initialization Script for MAVSDK_Drone_Show SITL Instances
+# Author: Alireza Ghaderi
+# GitHub: https://github.com/alireza787b/mavsdk_drone_show
+# Date: April 2024
+#
+# This script automates the creation and setup of multiple Docker containers for simulating
+# different SITL (Software in the Loop) instances of the MAVSDK_Drone_Show project. It 
+# initializes each container with a specific branch of the project repository, facilitating
+# parallel simulations with varied code bases.
+#
+# Usage:
+# ./create_docker.sh <number_of_instances> [branch_name]
+#
+# Arguments:
+#   number_of_instances - Required. Specifies the number of Docker containers to create.
+#   branch_name - Optional. Specifies the GitHub branch to be used for each container. Defaults to 'main'.
+#
+# Requirements:
+#   Docker must be installed and running on the host machine. The script assumes access to
+#   a Docker image named 'drone-template-1' that is pre-configured to run the SITL simulations.
+#
+# Example:
+#   To create 5 containers using the 'development' branch of the repository:
+#   ./create_docker.sh 5 development
+#
+# Note:
+#   This script will create an empty '.hwID' file for each container, copy it to the container,
+#   and then clean up by removing the file locally. It uses a basic loop and assumes that each
+#   container is named sequentially as 'drone-1', 'drone-2', etc.
+# *****************************************************************************************
+
+echo "Welcome to the Docker Initialization Script for MAVSDK_Drone_Show!"
+
 # Get the number of instances as input
 num_instances=$1
+branch_name=${2:-main}  # Default to 'main' if no branch is specified
 
 # Check if the number of instances is provided
 if [ -z "$num_instances" ]
@@ -18,8 +53,8 @@ do
     # Create an empty .hwID file in your local directory
     touch $i.hwID
 
-    # Run the Docker container
-    docker run --name drone-$i -d drone-template-1 bash /root/mavsdk_drone_show/multiple_sitl/startup_sitl.sh
+    # Run the Docker container, pass the branch name to the startup script
+    docker run --name drone-$i -d drone-template-1 bash /root/mavsdk_drone_show/multiple_sitl/startup_sitl.sh '' $branch_name
 
     # Give Docker a moment to get the container up and running
     sleep 3
