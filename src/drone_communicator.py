d3736b2a8b2d39fd92d203a8ab2f5e7aaf68246f
Alireza Ghaderi <p30planets@gmail.com>
2024-06-09 14:36:08 +0330

    Update drone_communicator.py

--- src/drone_communicator.py
+++ src/drone_communicator.py
@@ -150,7 +150,7 @@ class DroneCommunicator:
             drone_state = self.get_drone_state()
 
             # Create a struct format string based on the data types
-            telem_struct_fmt = self.params.telem_packet_size 
+            telem_struct_fmt = self.params.telem_struct_fmt 
             packet = struct.pack(telem_struct_fmt,
                                     77,
                                     drone_state['hw_id'],
