dc3d7b7872acdd486525ea33dc5b696ecf0680b8
Alireza Ghaderi <p30planets@gmail.com>
2024-07-05 17:09:45 +0300

    fixed urls

--- app/dashboard/drone-dashboard/src/services/droneApiService.js
+++ app/dashboard/drone-dashboard/src/services/droneApiService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { getBackendURL } from '../utilities';
+import { getBackendURL } from '../utilities/utilities';
 
 export const sendDroneCommand = async (commandData) => {
   try {
