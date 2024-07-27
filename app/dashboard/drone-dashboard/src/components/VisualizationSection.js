2243b3657eb8a0de5b1330d07d15e8108c81feaf
Alireza Ghaderi <p30planets@gmail.com>
2024-07-27 14:48:19 +0330

    visual light

--- app/dashboard/drone-dashboard/src/components/VisualizationSection.js
+++ app/dashboard/drone-dashboard/src/components/VisualizationSection.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { getBackendURL } from '../utilities/utilities'; // Ensure this utility function correctly returns the backend URL
+import { getBackendURL } from '../utilities/utilities';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css'; // Import the CSS for lightbox
 
 const VisualizationSection = ({ uploadCount }) => {
     const [plots, setPlots] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [currentImage, setCurrentImage] = useState(null);
 
     useEffect(() => {
         const fetchPlots = async () => {
@@ -27,24 +31,29 @@ const VisualizationSection = ({ uploadCount }) => {
         };
 
         fetchPlots();
-    }, [uploadCount]); // Dependency on uploadCount to refetch when new uploads happen
+    }, [uploadCount]);
+
+    const openModal = (image) => {
+        setCurrentImage(`${getBackendURL()}/get-show-plots/${encodeURIComponent(image)}`);
+        setModalIsOpen(true);
+    };
 
     return (
         <div className="visualization-section">
             <h2>Visualization of Drone Paths</h2>
             {loading && <p>Loading plots...</p>}
             {error && <p className="error">Error: {error}</p>}
-            <div className="plot-container">
+            <div>
                 {plots.length > 0 ? (
                     <>
                         {plots.filter(name => name === "all_drones.png").map((plot, index) => (
-                            <div key={index} className="plot-full-width">
+                            <div key={index} className="plot-full-width" onClick={() => openModal(plot)}>
                                 <img src={`${getBackendURL()}/get-show-plots/${encodeURIComponent(plot)}`} alt="All Drones" />
                             </div>
                         ))}
                         <div className="plot-grid">
                             {plots.filter(name => name !== "all_drones.png").map((plot, index) => (
-                                <div key={index} className="plot">
+                                <div key={index} className="plot" onClick={() => openModal(plot)}>
                                     <img src={`${getBackendURL()}/get-show-plots/${encodeURIComponent(plot)}`} alt={`Plot ${index}`} />
                                 </div>
                             ))}
@@ -54,6 +63,12 @@ const VisualizationSection = ({ uploadCount }) => {
                     <p>No plots available to display.</p>
                 )}
             </div>
+            {modalIsOpen && (
+                <Lightbox
+                    mainSrc={currentImage}
+                    onCloseRequest={() => setModalIsOpen(false)}
+                />
+            )}
         </div>
     );
 };
