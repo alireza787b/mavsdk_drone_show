443f1e42c0b361645039d30d05acdabf2edd4d2f
Alireza Ghaderi <p30planets@gmail.com>
2024-07-30 03:33:50 +0000

    sngle flask

--- gcs-server/app.py
+++ gcs-server/app.py
@@ -1,4 +1,3 @@
-#gcs-server/app.py
 import os
 import sys
 import logging
@@ -6,25 +5,31 @@ from flask import Flask
 from flask_cors import CORS
 from routes import setup_routes
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Adjust based on your application structure
-PLOTS_DIR = os.path.join(BASE_DIR, 'shapes/swarm/plots')
-
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-from params import Params
+# Configure logging at the entry point of the application
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def create_app():
+    """
+    Create and configure an instance of the Flask application.
+    Applies CORS to all routes within the application and sets up routes using a separate module.
+    """
     app = Flask(__name__)
-    CORS(app)  # This will enable CORS for all routes
+
+    # Enable CORS globally across all origins
+    CORS(app)
+
+    # Set up routes defined in the 'routes.py' module
     setup_routes(app)
+
     return app
 
 if __name__ == "__main__":
-    # Set up logging
-    logging.basicConfig(level=logging.INFO)
-    
     app = create_app()
 
+    # Load system or application parameters from external sources if needed
+    from params import Params  # Consider moving imports to the top if Params is used elsewhere outside __main__
+
+    # Adjust the running port and debug mode based on environmental settings
     if Params.env_mode == 'development':
         app.run(host='0.0.0.0', port=Params.flask_telem_socket_port, debug=True)
     else:
--- gcs-server/routes.py
+++ gcs-server/routes.py
@@ -177,5 +177,3 @@ def setup_routes(app):
         except Exception as e:
             logger.error(f"Failed to list directory: {e}")
             return jsonify({'error': str(e)}), 500
-
-
