48d542843990248633465c56c637cebd07893b6d
Alireza Ghaderi <p30planets@gmail.com>
2024-08-30 18:10:11 +0330

    sidebar

--- app/dashboard/drone-dashboard/src/components/SidebarMenu.js
+++ app/dashboard/drone-dashboard/src/components/SidebarMenu.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem, SubMenu, ProSidebarProvider } from 'react-pro-sidebar';
 import { FaTachometerAlt, FaGem, FaList, FaGithub, FaBars } from 'react-icons/fa';
-import 'react-pro-sidebar/dist/styles.css';
+import { Link } from 'react-router-dom';
 import '../styles/SidebarMenu.css';
 import CurrentTime from './CurrentTime';
 import GitInfo from './GitInfo';
@@ -27,62 +27,60 @@ const SidebarMenu = () => {
   const [collapsed, setCollapsed] = useState(window.innerWidth < 768);
 
   const menuItemStyles = {
-    icon: {
+    button: {
       color: themes[theme].menu.icon,
-    },
-    hover: {
-      backgroundColor: themes[theme].menu.hover.backgroundColor,
-      color: themes[theme].menu.hover.color
+      '&:hover': {
+        backgroundColor: themes[theme].menu.hover.backgroundColor,
+        color: themes[theme].menu.hover.color
+      }
     }
   };
 
   return (
-    <div style={{ display: 'flex', height: '100vh' }}>
-      <Sidebar
-        collapsed={collapsed}
-        backgroundColor={themes[theme].sidebar.backgroundColor}
-        color={themes[theme].sidebar.color}
-        collapsedWidth={"80px"}
-        className='sidebar'
-      >
-        <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-          <div className="sidebar-header">
-            <h3>Swarm Dashboard v0.9</h3>
-            <FaBars
-              className="toggle-icon"
-              onClick={() => setCollapsed(!collapsed)}
-            />
-          </div>
+    <ProSidebarProvider>
+      <div style={{ display: 'flex', height: '100vh' }}>
+        <Sidebar
+          collapsed={collapsed}
+          backgroundColor={themes[theme].sidebar.backgroundColor}
+          color={themes[theme].sidebar.color}
+          collapsedWidth={"80px"}
+        >
+          <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+            <FaBars className='FaBars-icon' onClick={() => setCollapsed(!collapsed)} />
+            <br />
 
-          <div className="sidebar-content">
-            <CurrentTime />
-            <Menu menuItemStyles={menuItemStyles}>
-              <MenuItem icon={<FaTachometerAlt />}>
-                Dashboard
-              </MenuItem>
-              <MenuItem icon={<FaGem />}>
-                Mission Config
-              </MenuItem>
-              <MenuItem icon={<FaList />}>
-                Swarm Design
-              </MenuItem>
-              <MenuItem icon={<FaGithub />}>
-                Manage Drone Show
-              </MenuItem>
-            </Menu>
-          </div>
+            <div className="sidebar-header">
+              <h3>Swarm Dashboard v0.9</h3>
+            </div>
 
-          <div className="sidebar-footer">
-            <GitInfo />
-            <p>&#169; {new Date().getFullYear()} MAVSDK Drone Show</p>
-            <a href="https://linkedin.com/in/alireza787b" target="_blank" rel="noopener noreferrer">
-              Linkedin
-            </a>
+            <div className="sidebar-time">
+              <CurrentTime />
+            </div>
+
+            <br />
+
+            <div className='menu-list'>
+              <Menu menuItemStyles={menuItemStyles}>
+                <Link to="/"><MenuItem icon={<FaTachometerAlt />}> Dashboard </MenuItem></Link>
+                <Link to="/mission-config"><MenuItem icon={<FaGem />}> Mission Config </MenuItem></Link>
+                <Link to="/swarm-design"><MenuItem icon={<FaList />}> Swarm Design </MenuItem></Link>
+                <Link to="/manage-drone-show"><MenuItem icon={<FaGithub />}> Manage Drone Show </MenuItem></Link>
+              </Menu>
+            </div>
+
+            {/* Git Information */}
+            <GitInfo />  {/* Integrating GitInfo component into the sidebar */}
+
+            {/* Footer */}
+            <div className="developer-info">
+              <p>&#169; {new Date().getFullYear()} <a href="https://github.com/alireza787b/mavsdk_drone_show" target='_blank'>MAVSDK Drone Show</a><br /> All rights reserved.</p>
+              <a href='https://linkedin.com/in/alireza787b' target='_blank'>Linkedin</a>
+            </div>
           </div>
-        </div>
-      </Sidebar>
-    </div>
+        </Sidebar>
+      </div>
+    </ProSidebarProvider>
   );
-};
+}
 
 export default SidebarMenu;
