a2ff89b1e90cec61e89d2161443e88e0b0c740d8
Alireza Ghaderi <p30planets@gmail.com>
2024-08-30 18:02:27 +0330

    new plugin sidebar

--- app/dashboard/drone-dashboard/src/components/GitInfo.js
+++ app/dashboard/drone-dashboard/src/components/GitInfo.js
@@ -1,46 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/GitInfo.css';
-import { getGitStatusURL } from '../utilities/utilities';  // Import the function to get the correct URL
+import { getGitStatusURL } from '../utilities/utilities';
 
 const GitInfo = () => {
   const [gitInfo, setGitInfo] = useState({});
   const [expanded, setExpanded] = useState(false);
-  const [loading, setLoading] = useState(true); // Add a loading state
-  const [error, setError] = useState(null); // Add an error state
 
   useEffect(() => {
     async function fetchGitInfo() {
       try {
-        setLoading(true); // Start loading
-        const response = await fetch(getGitStatusURL());  // Use the utility function to get the correct URL
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
+        const response = await fetch(getGitStatusURL());
         const data = await response.json();
         setGitInfo(data);
       } catch (error) {
-        setError(error.message); // Set error state
         console.error('Failed to fetch Git status:', error);
-      } finally {
-        setLoading(false); // End loading
       }
     }
 
     fetchGitInfo();
   }, []);
 
-  if (loading) {
-    return <div className="git-info-container">Loading...</div>; // Show loading state
-  }
-
-  if (error) {
-    return <div className="git-info-container">Error: {error}</div>; // Show error state
-  }
-
   return (
     <div className="git-info-container">
       <div className="git-info-summary" onClick={() => setExpanded(!expanded)}>
-        <div><strong>Branch:</strong> {gitInfo.branch}</div><br />
+        <div><strong>Branch:</strong> {gitInfo.branch}</div>
         <div><strong>Commit:</strong> {gitInfo.commit?.slice(0, 7)}...</div>
         <div className="toggle-details">{expanded ? '▲' : '▼'}</div>
       </div>
@@ -48,6 +31,7 @@ const GitInfo = () => {
       {expanded && (
         <div className="git-info-details">
           <ul>
+            <li><strong>Branch:</strong> {gitInfo.branch}</li>
             <li><strong>Commit:</strong> {gitInfo.commit}</li>
             <li><strong>Author:</strong> {gitInfo.author_name} ({gitInfo.author_email})</li>
             <li><strong>Date:</strong> {gitInfo.commit_date}</li>
