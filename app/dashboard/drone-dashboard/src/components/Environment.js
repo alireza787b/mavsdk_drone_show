b27c4d06670703cd7285853f7896e40a5ce1eaf3
Alireza Ghaderi <p30planets@gmail.com>
2024-10-22 18:11:35 +0330

    fix revert

--- app/dashboard/drone-dashboard/src/components/Environment.js
+++ app/dashboard/drone-dashboard/src/components/Environment.js
@@ -5,7 +5,7 @@ import { RepeatWrapping, TextureLoader } from 'three';
 import { TEXTURE_REPEAT, WORLD_SIZE } from '../utilities/utilities';
 
 const Environment = ({ groundLevel }) => {
-  const grassTexture = useLoader(TextureLoader, '/textures/grass1.jpg');
+  const grassTexture = useLoader(TextureLoader, '/grass1.jpg');
   grassTexture.wrapS = grassTexture.wrapT = RepeatWrapping;
   grassTexture.repeat.set(TEXTURE_REPEAT, TEXTURE_REPEAT);
 
