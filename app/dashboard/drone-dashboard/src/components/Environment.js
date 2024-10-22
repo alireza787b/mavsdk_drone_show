d4cf7cb148f5984ae06ed4f6a617c87c70f47bfe
Alireza Ghaderi <p30planets@gmail.com>
2024-10-22 18:17:09 +0330

    rev

--- app/dashboard/drone-dashboard/src/components/Environment.js
+++ app/dashboard/drone-dashboard/src/components/Environment.js
@@ -1,25 +1,24 @@
-// src/components/Environment.js
 import React from 'react';
-import { useLoader } from '@react-three/fiber';
-import { RepeatWrapping, TextureLoader } from 'three';
-import { TEXTURE_REPEAT, WORLD_SIZE } from '../utilities/utilities';
+import { useLoader } from '@react-three/fiber';  // Removed TextureLoader from this import
+import { RepeatWrapping, TextureLoader } from 'three';  // Added TextureLoader from 'three' instead
+import { TEXTURE_REPEAT, WORLD_SIZE} from '../utilities/utilities';
+
+
 
 const Environment = ({ groundLevel }) => {
   const grassTexture = useLoader(TextureLoader, '/grass1.jpg');
   grassTexture.wrapS = grassTexture.wrapT = RepeatWrapping;
   grassTexture.repeat.set(TEXTURE_REPEAT, TEXTURE_REPEAT);
 
-  
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, groundLevel, 0]} receiveShadow>
-      <planeGeometry args={[WORLD_SIZE, WORLD_SIZE]} />
-      <meshStandardMaterial
-        map={grassTexture}
-        side={2}
-        transparent={true}
-        opacity={0.5}
-      />
-    </mesh>
+    <>
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow castShadow>
+        <planeGeometry attach="geometry" args={[WORLD_SIZE, WORLD_SIZE]} />
+        <meshStandardMaterial map={grassTexture} attach="material" side={2} />
+      </mesh>
+      <ambientLight intensity={0.6} />
+      <hemisphereLight skyColor={'#ffffff'} groundColor={'#000000'} intensity={1} />
+    </>
   );
 };
 
