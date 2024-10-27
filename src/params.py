2f702218fe83ff8f2451d488cdb75978d5d363b9
Alireza Ghaderi <p30planets@gmail.com>
2024-10-27 09:22:06 +0000

    sim mode

--- src/params.py
+++ src/params.py
@@ -9,7 +9,7 @@ class Params():
     swarm_url = 'https://nb1.joomtalk.ir/download/swarm.csv'  # URL for the swarm file
 
     # Simulation and Mode Switches
-    sim_mode = False  # Set to True for simulation mode, False for real-life mode
+    sim_mode = True  # Set to True for simulation mode, False for real-life mode
     GIT_AUTO_PUSH = True
     GIT_REPO_URL = 'git@github.com:alireza787b/mavsdk_drone_show.git'
     
