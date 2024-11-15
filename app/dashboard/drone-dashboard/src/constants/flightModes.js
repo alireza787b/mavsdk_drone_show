1a0e365b892bc5bc502f33bd88fb71ee16a2a32d
Alireza Ghaderi <p30planets@gmail.com>
2024-11-15 10:51:31 +0330

    selective dornes command

--- app/dashboard/drone-dashboard/src/constants/flightModes.js
+++ app/dashboard/drone-dashboard/src/constants/flightModes.js
@@ -2,14 +2,12 @@
 export const FLIGHT_MODES = {
     458752: 'Stabilized',
     196608: 'Position',
-    100925440: 'Land',
     393216: 'Offboard',
     50593792: 'Hold',
     84148224: 'Return',
     131072: 'Altitude',
     65536: 'Manual',
     327680: 'Acro',
-    29: 'Hold',
     157: 'Take-off',
-    192: 'Land',
+    192: 'Hold',
 };
