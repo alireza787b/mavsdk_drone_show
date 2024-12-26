c67eabfd74837b25bfcd87adbd920afa3e993510
Alireza Ghaderi <p30planets@gmail.com>
2024-12-26 10:34:22 +0300

    rev

--- app/dashboard/drone-dashboard/src/pages/MissionConfig.js
+++ app/dashboard/drone-dashboard/src/pages/MissionConfig.js
@@ -145,6 +145,7 @@ const MissionConfig = () => {
         const response = await axios.get(`${backendURL}/git-status`);
         setGitStatusData(response.data);
         console.log('Git Status Data Fetched:', response.data); // Debugging
+        console.log('Git Status Data Fetched 1 :', response.data[0]); // Debugging
       } catch (error) {
         console.error('Error fetching Git status data:', error);
       }
