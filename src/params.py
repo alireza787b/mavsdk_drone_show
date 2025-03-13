7866b5e2e25443065f0ba2ee328794a233642178
Alireza Ghaderi <p30planets@gmail.com>
2025-03-13 15:04:02 +0330

    mil gcs ip

--- src/params.py
+++ src/params.py
@@ -112,6 +112,7 @@ class Params:
     extra_devices = [
         f"127.0.0.1:{local_mavlink_port}",
         f"127.0.0.1:{local_mavlink2rest_port}",
+        "100.93.44.185:14550",
         "100.93.169.180:14550",
         "100.93.214.147:14550"
     ]  # Extra devices for MAVLink routing
