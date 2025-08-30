24888957b55cb7bf1b17868436d725b03b765120
Alireza Ghaderi <p30planets@gmail.com>
2025-08-30 11:19:06 +0330

    doc port

--- app/linux_dashboard_start.sh
+++ app/linux_dashboard_start.sh
@@ -54,7 +54,7 @@ ENABLE_AUTO_PULL=true  # Enable or disable the automatic pulling and syncing of 
 # Configurable Variables
 SESSION_NAME="DroneServices"
 GCS_PORT=5000
-GUI_PORT=3000
+GUI_PORT=3030
 VENV_PATH="$HOME/mavsdk_drone_show/venv"
 UPDATE_SCRIPT_PATH="$HOME/mavsdk_drone_show/tools/update_repo_ssh.sh"  # Path to the repo update script
 BRANCH_NAME="main-candidate"  # Set default branch to main-candidate
