06e44d0252295a9bd22a6ec8d8eb7fb7d602e215
Alireza Ghaderi <p30planets@gmail.com>
2026-04-26 02:23:30 +0000

    refactor(dashboard): use operator actions for briefing export

--- app/dashboard/drone-dashboard/src/components/BriefingExport.js
+++ app/dashboard/drone-dashboard/src/components/BriefingExport.js
@@ -2,10 +2,10 @@
 
 import React, { useState } from 'react';
 import { generateKML } from '../utilities/missionConfigUtilities';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload, faPrint } from '@fortawesome/free-solid-svg-icons';
+import { FaDownload, FaPrint } from 'react-icons/fa';
 import OriginModal from './OriginModal';
-import '../styles/BriefingExport.css'; // Ensure all styles are in MissionConfig.css
+import { ActionIconButton } from './ui/OperatorPrimitives';
+import '../styles/BriefingExport.css';
 
 const BriefingExport = ({ configData, originLat, originLon, setOriginLat, setOriginLon }) => {
   const [showOriginModal, setShowOriginModal] = useState(false);
@@ -51,12 +51,23 @@ const BriefingExport = ({ configData, originLat, originLon, setOriginLat, setOri
     <div className="briefing-export-container">
       {/* Buttons for Exporting to KML and Printing Mission Briefing */}
       <div className="briefing-actions">
-        <button className="export-kml" onClick={exportToKML} title="Export drone positions to KML">
-          <FontAwesomeIcon icon={faDownload} /> Export to Google Earth (KML)
-        </button>
-        <button className="print-mission" onClick={handlePrint} title="Print the mission briefing">
-          <FontAwesomeIcon icon={faPrint} /> Print Mission Briefing
-        </button>
+        <ActionIconButton
+          icon={<FaDownload />}
+          label="Export drone positions to KML"
+          tone="info"
+          onClick={exportToKML}
+          data-help="Export drone positions to KML"
+        >
+          KML
+        </ActionIconButton>
+        <ActionIconButton
+          icon={<FaPrint />}
+          label="Print mission briefing"
+          onClick={handlePrint}
+          data-help="Print the mission briefing"
+        >
+          Print
+        </ActionIconButton>
       </div>
 
       {/* Origin Modal */}
